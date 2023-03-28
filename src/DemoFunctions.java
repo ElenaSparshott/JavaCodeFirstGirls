563dc513ae982f55b049b73d646a3c1198ec6531
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-03-28 21:39:09 +0100

    arrays

--- src/DemoFunctions.java
+++ src/DemoFunctions.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class DemoFunctions {
 
-    public static String getName(){
+    public static String getName(int userNumber){
         Scanner inputGetter = new Scanner(System.in);
-        String askForName = "What is your name?";
+        String askForName = "What is your name (user " +userNumber+")?";
         String greeting = "Hello ";
         String userName;
 
@@ -16,11 +16,32 @@ public class DemoFunctions {
         return userName;
     }
 
-//    public static char[][] createBoard(){
-//
-//    }
+
+
+    public static int[] createArray() {
+        int[] tempArray = {1,2,3};
+        return tempArray;
+    }
+
+    public static int[] createArray2() {
+        int[] tempArray = new int[3];
+        tempArray[0] = 4;
+        tempArray[1] = 5;
+        tempArray[2] = 6;
+        return tempArray;
+    }
+
 
     public static void main(String[] args) {
-        getName();
+        int number = 2;
+        String username = getName(number);
+        System.out.println("Hello " + username);
+
+        int[] numbers = createArray();
+        int[] array_again = createArray2();
+
+        displayArray(numbers);
+        displayArray(array_again);
+
     }
 }
