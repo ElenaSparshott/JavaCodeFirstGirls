fb8ed542f4274b597a38ee0f85b5c1e1cc96abfd
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-03-30 19:43:37 +0100

    fixed for loop for print board

--- src/Board.java
+++ src/Board.java
@@ -2,35 +2,47 @@ import java.util.Scanner;
 
 public class Board {
 
+    private char[][] board;
+    private char  defaultValue = '*';
+
+    public Board(){
+        System.out.println("initialising");
+        createBoard();
+    }
+
+    public void displayBoard(){
+
+
+
+        for(int row=0; row<3; row++) {
+            String row_str = "";
+            for (int col = 0; col < 3; col++) {
+                row_str+= " | " + board[row][col];
+            }
+            System.out.println(row_str);
+        }
+    }
+
     public char[][] createBoard(){
-        char [][] board = new char[3][3];
+
+        board = new char[3][3];
         for(int row=0; row<3; row++){
             for(int col=0; col<3; col++){
-                board[row][col] = ' ';
+                board[row][col] = defaultValue;
             }
         }
         return board;
     }
 
-    public void displayBoard(char[][] board){
-        for(int row=0; row<3; row++) {
-            String row_str = "";
-            for (int col = 0; col < 3; col++) {
-                System.out.println(row_str+= " | " + board[row][col]);
-            }
-        }
-    }
 
     public void gameLoop(){
-
-        char [][] newBoard = createBoard();
 
         Scanner input = new Scanner(System.in);
         System.out.println("Do you want to display the board (yes or No)");
         String userAnswer = input.nextLine();
 
         while(userAnswer.equals("yes")) {
-            displayBoard(newBoard);
+            displayBoard();
             System.out.println("Do you want to display the board (yes or No)");
             userAnswer = input.nextLine();
         }
@@ -40,6 +52,7 @@ public class Board {
 
     public static void main(String[] args) {
         Board testBoard = new Board();
+        testBoard.displayBoard();
         testBoard.gameLoop();
     }
 }
